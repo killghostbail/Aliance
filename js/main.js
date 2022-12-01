5b2187517a9a0a927e7279c424e726d41057b8a5
killghostbail <killghostbail@gmail.com>
2022-12-01 13:41:42 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
-window.addEventListener("scroll", () =>{
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light")
-    logo.href.baseVal = "img/sprites.svg#logo"; 
-  } else {
-    navbar.classList.remove("navbar-light")
-    logo.href.baseVal = "img/sprites.svg#logo-light"; 
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo";
+  } else {
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo-light";
   }
-  }) ;
+});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open");
+});
