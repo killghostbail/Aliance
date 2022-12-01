5578e8717f630244d7b25efea629942ea1cdf888
killghostbail <killghostbail@gmail.com>
2022-12-01 16:57:05 +0300

    fix: toggle menu, resposive slider in features

--- js/main.js
+++ js/main.js
@@ -3,16 +3,63 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprites.svg#logo";
+};
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprites.svg#logo-light";
+};
+
+const openMenu = (event) => {
+  // menu opening function
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu")
+  document.body.style.overflow = "hidden"; // restrict site scrolling
+  lightModeOn();
+};
+const closeMenu = (event) => {
+  // menu closing function
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu")
+  document.body.style.overflow = ""; // allow site scrolling
+  lightModeOff();
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo-light";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+      centeredSlides: true,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
